68dd015f30940fe1ae790da89be695a69ecef6e9
uditnm <udit20001@gmail.com>
2025-12-20 16:18:14 +0530

    Changes: read command input

--- app/main.go
+++ app/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
-	"fmt"
+	"fmt",
+	"bufio",
+	"os"
 )
 
 var _ = fmt.Print
 
 func main() {
 	fmt.Print("$ ")
+	reader := bufio.NewReader(os.Stdin)
+
+	input, err := reader.ReadString('\n')
+
+	fmt.Print(input[:len(input) - 1] + ": command not found")
 }
